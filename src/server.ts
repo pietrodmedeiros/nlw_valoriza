2ed26305a2b6ec188342f35e6c93dbfc47756a47
Pietro de Deus Medeiros <spogmedeiros@gmail.com>
2021-06-22 20:11:57 -0300

    first database and create users

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,9 @@
-import express, { response } from "express";
+
+import "reflect-metadata"
+import express from "express"
+import "./database"
 
 const app = express()
-
-app.get("/test", (request, response) => {
-    return response.send("Hello there! GET method working!")
-})
-
-app.post("/test-post", (request, response) =>{
-    return response.send("Hello there! POST method working!")
-})
-
-
-
 
 app.listen(3333, () => console.log("YES!!! o/ Server is running!"))
 
