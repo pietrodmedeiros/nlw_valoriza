08c18b771a809f17e7360f9fa953ea94789054dd
Pietro de Deus Medeiros <spogmedeiros@gmail.com>
2021-06-24 12:30:57 -0300

    add crypto to password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,5 +1,7 @@
 import { UsersRepositories } from "../repositories/UsersRepositories"
 import {getCustomRepository} from "typeorm"
+import {hash} from "bcryptjs"
+
 interface IUserRequest {
     name: string
     email: string
@@ -26,11 +28,13 @@ class CreateUserService {
             throw new Error("User already exists!")
         }
 
+        const passwordHash = await hash(password, 8)
+
         const user = usersRepository.create({
             name, 
             email,
             admin,
-            password
+            password: passwordHash
         })
         await usersRepository.save(user)
 
