32ef30192a4b7e5a7e7ec7daf69c061669d845d6
Pietro de Deus Medeiros <spogmedeiros@gmail.com>
2021-06-25 17:10:14 -0300

    Add ensureAuthenticated and ensureAdmin.

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateTagController } from "./controllers/CrateTagController"
 import { ensureAdmin } from "./middlewares/ensureAdmin"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 import { CreateComplimentController } from "./controllers/CreateComplimentController"
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated"
 
 const router = Router()
 const createUserController = new CreateUserController()
@@ -12,8 +13,8 @@ const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
 
 router.post("/users", createUserController.handle)
-router.post("/tags", ensureAdmin, createTagController.handle)
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
-router.post("/compliments", createComplimentController.handle)
+router.post("/compliments", ensureAuthenticated, createComplimentController.handle)
 
 export {router}
